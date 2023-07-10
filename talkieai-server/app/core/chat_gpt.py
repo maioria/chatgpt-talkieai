58ae77279bfc80405f1ddaeb14eda57151af13d0
zhangtao <zhangtaoj2ee@163.com>
2023-07-10 14:35:48 +0800

    commit.

--- talkieai-server/app/core/chat_gpt.py
+++ talkieai-server/app/core/chat_gpt.py
@@ -1,20 +1,10 @@
 import json
 from abc import ABC, abstractmethod
 
-from app.config import Config
 from app.core.logging import logging
-import os
 from typing import Dict, List
 import openai
 from pydantic import BaseModel, constr
-
-# 设置HTTP和HTTPS代理
-# proxy_host = "127.0.0.1"
-# proxy_port = "33210"
-# os.environ["HTTP_PROXY"] = f"http://{proxy_host}:{proxy_port}"
-# os.environ["HTTPS_PROXY"] = f"http://{proxy_host}:{proxy_port}"
-os.environ["HTTP_PROXY"] = ''
-os.environ["HTTPS_PROXY"] = ''
 
 """chat_gpt相关调用"""
 
@@ -55,27 +45,3 @@ class ChatGptLocalComponent(ChatGptComponent):
         result = response.choices[0].message.content
         logging.info(f'response:{result}')
         return {'data': result}
-
-
-class ChatGptRemoteComponent(ChatGptComponent):
-    """远程调用chatgpt"""
-
-    def __init__(self):
-        import requests
-        session = requests.Session()
-        session.trust_env = False
-        self.session = session
-
-    def invoke_chat(self, dto: ChatGPTInvokeDTO, api_key: ApiKeyModel) -> Dict:
-        logging.info(f'request:{dto.dict()}')
-        headers = {
-            "Content-Type": "application/json",
-            "Api-Key": api_key.api_key,
-            "Organization": api_key.organization
-        }
-        url = f'{Config.CHAT_GPT_SERVER}/api/v1/chat'
-        remote_result = self.session.post(url, data=json.dumps(dto.dict()), headers=headers)
-        result = remote_result.text
-        logging.info(f'response:{result}')
-        json_result = json.loads(result)
-        return json_result
